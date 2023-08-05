fb949bec3d092bb6ac0d2a6dc347810738bdadc6
mkhalil7 <mk.ouachikh@gmail.com>
2023-08-05 15:01:09 +0200

    Explanatory comment

--- project/contracts/DirhamConverter.sol
+++ project/contracts/DirhamConverter.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 
 
 contract DirhamConverter {
+
+    // we are using * 100 actual rate to avoid the issue with fixed point numbers in Solidity. The division is done later in the the convert functions
     uint euroConversionRate = 9;
     uint dollarConversionRate = 10; 
     uint poundConversionRate = 8; 
